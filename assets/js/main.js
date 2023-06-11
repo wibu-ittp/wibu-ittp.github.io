56e2e6b16f0e312fb7dea18b4b88593913539ae1
ahmadmcer <ahmadmcer@gmail.com>
2023-06-12 00:57:43 +0700

    fix: improve showing join number

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,4 @@ const number = document.getElementById('number');
 
 fetch('https://sheetdb.io/api/v1/uzkvj9aa7hikd/count')
   .then((response) => response.json())
-  .then((data) => number.innerHTML = data.rows);
+  .then((data) => number.innerHTML = data.rows + " orang");
