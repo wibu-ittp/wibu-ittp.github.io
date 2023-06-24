389749ca1b2cab35303e44476c7d38544cc0f2ec
ahmadmcer <ahmadmcer@gmail.com>
2023-06-25 00:30:58 +0700

    feat: add loading animation and update fetch API config
    BREAKING CHANGE: changes Remix Icon to FontAwesome and update fetch API config

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,37 @@
+// Variables
 const number = document.getElementById('number');
+const loading = document.getElementById('loading');
 
-fetch('https://sheetdb.io/api/v1/uzkvj9aa7hikd/count')
+// Loading
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setOpacity(element, opacity) {
+  element.style.transition = 'opacity 1s';
+  element.style.opacity = opacity;
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setOpacity(loading, '0%');
+  setInterval(function() {
+    setVisible('#loading', false);
+  }, 1000);
+});
+
+// Fetch
+fetch('https://sheetdb.io/api/v1/uzkvj9aa7hikd/count', {
+  method: 'GET',
+  headers: {'Authorization': 'Bearer y790s151wmzi5zycqanb7ccz3rmw26vukfpq2g42'}
+})
   .then((response) => response.json())
   .then((data) => number.innerHTML = data.rows + " 人 / orang");
